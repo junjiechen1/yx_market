877df23e85028455d9e439d9a78fb65450e35846
junjiechen1 <37597962+junjiechen1@users.noreply.github.com>
2019-11-01 15:10:27 -0400

    android studio version of yx_marketplace

---
+++ yx_marketplace_firebase/app/src/main/java/com/internal/yx_marketplace_firebase/MainActivity.java
@@ -0,0 +1,16 @@
+package com.internal.yx_marketplace_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Firebase connection success",Toast.LENGTH_LONG).show();
+    }
+}
